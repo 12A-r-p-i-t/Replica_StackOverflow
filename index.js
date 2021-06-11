b55a3a6281e53cf65e78128ef3823e9717f74967
Arpit Shukla <shills.arpit68@gmail.com>
2021-06-11 13:36:14 +0530

    Divided the Project's routes in diffrent modules using Router

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const auth = require("./routes/api/auth");
+const profile = require("./routes/api/profile");
+const questions = require("./routes/api/questions");
 const app = express();
+
+// Middleware for bodyParser
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
 
 // mongoDB configuration
 const db = require("./setup/myurl").mongoURL;
@@ -24,10 +32,20 @@ mongoose
 
 
 // routes
+
+
+//just For testing
 app.get("/",function(req,res){
     res.send("oii");
 })
 
+// actual Routes
+
+app.use("/api/auth",auth);
+
+app.use("/api/profile",profile);
+
+app.use("/api/questions",questions);
 
 
 
